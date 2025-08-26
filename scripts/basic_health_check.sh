51fb8990898bc56aea6693886283fd3eafccbc47
Satwik Joel <86243758+nsatwik@users.noreply.github.com>
2025-08-26 22:43:07 +0530

    Update basic_health_check.sh

--- scripts/basic_health_check.sh
+++ scripts/basic_health_check.sh
@@ -3,7 +3,7 @@ set -e
 
 echo ">>> Running health check..."
 
-STATUS=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/SampleMavenTomcatApp/ || true)
+STATUS=$(curl -s -o /dev/null -w "%{http_code}" http://3.109.157.198:8080/SampleMavenTomcatApp/ || true)
 
 if [ "$STATUS" -eq 200 ]; then
     echo ">>> Health check passed. App is running."
